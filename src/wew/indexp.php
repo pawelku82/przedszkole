28aedcaa02e8191d423dedb9d5ef408a49a754f6
Jurann <jurand.pl@o2.pl>
2015-06-21 13:44:15 +0200

    Index
    14.06.2015

--- src/wew/indexp.php
+++ src/wew/indexp.php
@@ -1,3 +1,28 @@
 <?php
+session_start();
+include_once('klasy.php');
+if (!isset($rys1))
+	$rys1 = new rysowanie;	
+
+if (isset($_GET["logowanie"]))
+{
+	echo $rys1->sesja1->logowanie($_POST["user1"], sha1($_POST["pass1"]));
+	
+}
+else if (isset($_GET["menu11"]))
+{
+	if ($_GET["menu11"]==2)
+		$rys1->sesja1->wyloguj();
+		
+	$rys1->menu();
+	echo $rys1->wybor1;
+}
+else
+{
+	
+	$rys1->tresc();
+	echo $rys1->wybor1;
+	
+}
 
 ?>
