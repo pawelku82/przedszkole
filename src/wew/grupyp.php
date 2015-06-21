6edb234f6969f5310377f518f317592ec627e075
Jurann <jurand.pl@o2.pl>
2015-06-21 13:42:37 +0200

    Obsluga grup
    14.06.2015

--- src/wew/grupyp.php
+++ src/wew/grupyp.php
@@ -1,3 +1,34 @@
 <?php
+error_reporting(E_ERROR);
+session_start(); 
+include_once('klasy.php');
+if (!isset($rysp))
+	$rysp = new rysowanie_grupy;
+
+if (isset($_GET["wyloguj"]))
+{
+	$rysp->sesja1->wyloguj();
+
+}
+if (isset($_GET["klawisz"]))
+{
+	echo $rysp->rysuj_kdodaj();
+
+}
+if (isset($_GET["rysujtab"]))
+{
+	echo $rysp->rysuj_tabelagrupy();
+
+}
+if (isset($_GET["dodgrupy"]))
+{
+	echo $rysp->dodaj_grupy();
+
+}
+else if (isset($_GET["usungrupy"]))
+{
+	if ($rysp->sesja1->czyzalogowany()>0)
+		$rysp->usuwanie_grupy($_GET["usungrupy"]); 
+}	
 
 ?>
